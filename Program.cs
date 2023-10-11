a32fafc0434d1b6322e45f109a3a5dac76b5dc99
Harsh Arora <aroraharshit30@gmail.com>
2023-10-11 19:26:42 -0400

    fix program cs

--- Program.cs
+++ Program.cs
@@ -9,5 +9,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<IImageAnalysisService, ImageAnalysisService>();
 builder.Services.AddSingleton<IExtractTextService, ExtractTextService>();
-builder.Services.AddSingleton<ITextToSpeechService, TextToSpeechService>();
 await builder.Build().RunAsync();
